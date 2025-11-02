83f29837435c792e69099d0d197ce499bc57e3e5
bmalvo <boydmalvo@gmail.com>
2025-11-02 22:22:09 +0100

    create add_reminder_button

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from telegram import Update
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from token_bot import EXACT_TOKEN_TYPES
 
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def hello(update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        f'Hello {update.effective_user.first_name}', 
+        reply_markup=add_reminder_button)
+    # await update.message.reply_text(
+    #     'hello, creator!', reply_markup=add_reminder_button())
+
+def add_reminder_button():
+    keyboard = [[KeyboardButton('add a reminder ⏰')]]
+    return ReplyKeyboardMarkup(keyboard)
 
 
 app = ApplicationBuilder().token(EXACT_TOKEN_TYPES).build()
