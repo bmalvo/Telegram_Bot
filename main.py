7898325e8205553fa8d19c8708a7404c475d6823
bmalvo <boydmalvo@gmail.com>
2025-11-02 20:07:41 +0100

    change module name for token

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from token import token
+from token_bot import EXACT_TOKEN_TYPES
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token(token).build()
+app = ApplicationBuilder().token(EXACT_TOKEN_TYPES).build()
 
 app.add_handler(CommandHandler("hello", hello))
 
