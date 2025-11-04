c8f5c817b72493fbda879a77135632f94eb6f3f7
bmalvo <boydmalvo@gmail.com>
2025-11-04 22:29:19 +0100

    create conversation handler (nothing works)

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes, 
+                          ConversationHandler, MessageHandler, filters, Updater)
 from token_bot import EXACT_TOKEN_TYPES
 
+ENTER_MESSAGE, ENTER_TIME = 'range(2)', 'ee'
+
+
+def add_reminder_handler(update: Update, context: CallbackContext):
+    update.message.reply_text('Please enter a message of the reminder:')
+    return ENTER_MESSAGE
+
+conv_handler = ConversationHandler(
+
+    entry_points=[MessageHandler(filters.regex(ADD_REMINDER_TEXT), add_reminder_handler)],
+    states = {
+        ENTER_MESSAGE = [(MessageHandler(filters.all, enter_message_handler))],
+        ENTER_TIME = [MessageHandler(filters.all, enter_time_handler)]
+    },
+    fallbacks = []
+)
 
 async def hello(update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
@@ -11,10 +28,12 @@ async def hello(update, context: ContextTypes.DEFAULT_TYPE) -> None:
     #     'hello, creator!', reply_markup=add_reminder_button())
 
 def add_reminder_button():
-    keyboard = [[KeyboardButton('add a reminder ⏰')]]
+    keyboard = [[KeyboardButton(ADD_REMINDER_TEXT)]]
     return ReplyKeyboardMarkup(keyboard)
 
-
+updater = Updater(EXACT_TOKEN_TYPES, use_context= True)
+updater.dispatcher.add_handler(CommandHandler('start', start_handler))
+ 
 app = ApplicationBuilder().token(EXACT_TOKEN_TYPES).build()
 
 app.add_handler(CommandHandler("hello", hello))
